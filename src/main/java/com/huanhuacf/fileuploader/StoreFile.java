7ddaf5ed2f74942cc31a77386f975fcb6c130301
Chintsai Hwo <jcglqmoyx@gmail.com>
2021-02-27 00:10:08 +0800

    Initial commit

---
+++ src/main/java/com/huanhuacf/fileuploader/StoreFile.java
@@ -0,0 +1,33 @@
+package com.huanhuacf.fileuploader;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+
+@Controller
+public class StoreFile {
+    @PostMapping("/store")
+    @ResponseBody
+    public String upload(@RequestParam("file") MultipartFile file) {
+        if (file.isEmpty()) {
+            return "上传失败，请选择文件";
+        }
+
+        String fileName = file.getOriginalFilename();
+        String filePath = "/home/jcglqmoyx/";
+        System.out.println(filePath + fileName);
+        File dest = new File(filePath + fileName);
+        try {
+            file.transferTo(dest);
+            return "上传成功";
+        } catch (IOException e) {
+        }
+        return "上传失败！";
+    }
+}
